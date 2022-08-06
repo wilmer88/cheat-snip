22bba71d5fa684bd190c5b6c70b6a0df83be55ec
Wilmer Rivera <wilmerbaby@gmail.com>
2022-08-06 14:32:18 -0400

    created add page and if cases to layouts.html navbar links to be active if url matches link

--- app/snips/urls.py
+++ app/snips/urls.py
@@ -3,7 +3,9 @@ from . import views
 #the $ sign besides home restrict the path from having home plus characters and still hit the home path
 urlpatterns = [
  re_path(r'^index$', views.index, name='index'),
- re_path(r'^fight_stage$', views.fight_stage, name='fight_stage')
+ re_path(r'^fight_stage$', views.fight_stage, name='fight_stage'),
+ re_path(r'^add_snip$', views.add_snip, name="add_snip")
+
  ]
 # from django.urls import re_path, include
 # from django.contrib import admin
--- app/snips/views.py
+++ app/snips/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Snipit
+# monkey= datetime.datetime.now()
 def index(request):
+     
+        all_snips = Snipit.objects.all()
 
-       return render(request, "users/index.html", {
-        "things_to_do": ["learn c", 'learn python', 'find work']})
+        return render(request, "users/index.html", {
+               "navbar":"index", "language": all_snips
+  })
         
 def fight_stage(request):
         return render(request, 'users/fight_stage.html', {})
+
+def add_snip(request):
+        return render(request, "users/add_snip.html", {
+                "navbar":"add_snip"
+                })
         
         
 # "header": 'with c# ',
